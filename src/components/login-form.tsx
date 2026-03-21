3f29394aa67a13523eb86cc6ec1d98e1defb546c
Jonatan <jonatanjmissora@gmail.com>
2026-03-21 00:09:32 -0300

    empezando con el diseo del login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -79,17 +79,15 @@ export function LoginForm({
 	}
 
 	return (
-		<div
-			className={cn(
-				"min-w-1/4 flex flex-col gap-6 w-full sm:w-1/4 mx-auto",
-				className
-			)}
-			{...props}
-		>
-			<Card>
+		<div className={cn("w-90 relative ml-50", className)} {...props}>
+			<Card className="bg-white/80 border-transparent shadow-2xl">
 				<CardHeader className="text-center">
-					<CardTitle className="text-xl">Bienvenido de nuevo</CardTitle>
-					<CardDescription>Ingresa con una cuenta de Google</CardDescription>
+					<CardTitle className="text-xl text-background">
+						Bienvenido de nuevo
+					</CardTitle>
+					<CardDescription className="text-background/75">
+						Ingresa con una cuenta de Google
+					</CardDescription>
 				</CardHeader>
 				<CardContent>
 					<form
@@ -101,18 +99,27 @@ export function LoginForm({
 					>
 						<FieldGroup>
 							<Field>
-								<Button variant="outline" type="button" onClick={signIn}>
+								<Button
+									variant="outline"
+									type="button"
+									onClick={signIn}
+									className="text-background"
+								>
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 										<title>Google</title>
 										<path
 											d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-											fill="currentColor"
+											fill="red"
 										/>
 									</svg>
 									{loading ? "Iniciando..." : "Google"}
 								</Button>
 							</Field>
-							<FieldSeparator>O continua con</FieldSeparator>
+							<FieldSeparator>
+								<span className="text-accent bg-transparent">
+									O continua con
+								</span>
+							</FieldSeparator>
 
 							<form.Field
 								name="email"
@@ -121,8 +128,14 @@ export function LoginForm({
 										field.state.meta.isTouched && !field.state.meta.isValid
 									return (
 										<Field data-invalid={isInvalid}>
-											<FieldLabel htmlFor={field.name}>Email</FieldLabel>
+											<FieldLabel
+												htmlFor={field.name}
+												className="text-background"
+											>
+												Email
+											</FieldLabel>
 											<Input
+												className="text-background"
 												id={field.name}
 												name={field.name}
 												value={field.state.value}
@@ -146,9 +159,15 @@ export function LoginForm({
 										field.state.meta.isTouched && !field.state.meta.isValid
 									return (
 										<Field data-invalid={isInvalid}>
-											<FieldLabel htmlFor={field.name}>Contraseña</FieldLabel>
+											<FieldLabel
+												htmlFor={field.name}
+												className="text-background"
+											>
+												Contraseña
+											</FieldLabel>
 											<div className="relative">
 												<Input
+													className="text-background"
 													id={field.name}
 													name={field.name}
 													value={field.state.value}
@@ -179,8 +198,10 @@ export function LoginForm({
 							/>
 
 							<Field>
-								<Button type="submit">Ingresar</Button>
-								<FieldDescription className="text-center">
+								<Button type="submit" className="bg-green-600">
+									Ingresar
+								</Button>
+								<FieldDescription className="text-center text-background/65">
 									No tiene cuenta ?{" "}
 									<Link to="/register" viewTransition={{ types: ["rotateZ"] }}>
 										Registrate
--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,6 +1,5 @@
 import { LoginForm } from "@/components/login-form"
 import { createFileRoute } from "@tanstack/react-router"
-import SectionContainer from "@/components/layout/section-container"
 
 export const Route = createFileRoute("/login/")({
 	component: RouteComponent,
@@ -8,8 +7,27 @@ export const Route = createFileRoute("/login/")({
 
 function RouteComponent() {
 	return (
-		<SectionContainer>
+		<section className="w-full h-screen flex items-center relative overflow-hidden">
+			<img
+				src="./wall1.webp"
+				alt=""
+				className="absolute inset-0 w-full h-full object-contian"
+			/>
+			<div className="absolute inset-0 bg-linear-to-b from-white/75 to-transparent"></div>
+			<div className="bg-linear-to-b from-white/80 to-white/70 w-2/3 h-[200dvh] absolute -left-1/4 -bottom-80 -rotate-25 shadow-2xl"></div>
+			<div className="bg-linear-to-b from-white/70 via-white/50 to-transparent w-140 h-[200dvh] absolute right-45 -bottom-80 -rotate-25 shadow-2xl"></div>
+			<div className="absolute right-80 top-24 z-10 flex flex-col gap-3 justify-center items-center">
+				<div className=" flex gap-4 items-center justify-center">
+					<img src="./logo2.png" alt="" className="w-30" />
+					<h1 className="text-7xl font-bold tracking-widest text-green-700">
+						EnHySa
+					</h1>
+				</div>
+				<p className="text-lg italic text-center tracking-wider w-[80%] text-pretty text-accent/75">
+					Gestiona tus informes de iluminación y protocolo.
+				</p>
+			</div>
 			<LoginForm />
-		</SectionContainer>
+		</section>
 	)
 }
