b318772183805c62153a643e83a32ef562387945
Jonatan Missora <jonatanjmissora@gmail.com>
2026-04-08 15:28:44 -0300

    plan cascaron notebook responsive

--- src/routes/_protected/pricing.tsx
+++ src/routes/_protected/pricing.tsx
@@ -2,6 +2,45 @@ import { createFileRoute, Link } from "@tanstack/react-router"
 import { ArrowRight, Check, ChevronLeft, Shield } from "lucide-react"
 import { useState } from "react"
 
+const PLANS = [
+	{
+		title: "Gratis",
+		price: 0,
+		subtitle: "Lorem ipsum dolor sit amet.",
+		benefits: [
+			"beneficio adquirido 1",
+			"beneficio adquirido 2",
+			"beneficio adquirido 3",
+			"beneficio adquirido 4",
+			"beneficio adquirido 5",
+		],
+	},
+	{
+		title: "Profesional",
+		price: 25,
+		subtitle: "Lorem ipsum dolor sit amet.",
+		benefits: [
+			"beneficio adquirido 1",
+			"beneficio adquirido 2",
+			"beneficio adquirido 3",
+			"beneficio adquirido 4",
+			"beneficio adquirido 5",
+		],
+	},
+	{
+		title: "Empresarial",
+		price: 55,
+		subtitle: "Lorem ipsum dolor sit amet.",
+		benefits: [
+			"beneficio adquirido 1",
+			"beneficio adquirido 2",
+			"beneficio adquirido 3",
+			"beneficio adquirido 4",
+			"beneficio adquirido 5",
+		],
+	},
+]
+
 export const Route = createFileRoute("/_protected/pricing")({
 	component: RouteComponent,
 })
@@ -24,193 +63,79 @@ function RouteComponent() {
 				</div>
 			</header>
 
-			<div className="flex-1 flex justify-center items-center flex-col gap-6">
-				<div className="flex items-center gap-2  text-6xl font-bold tracking-wildest">
+			<div className="flex-1 flex justify-center items-center flex-col gap-6 	sm:py-10 2xl:py-20">
+				<div className="flex items-center gap-2  sm:text-5xl 2xl:text-6xl font-bold tracking-wildest relative">
 					<span>Planes</span>
-					<Shield className="size-14 text-amber-500/50" />
+					<Shield className="absolute top-1/2 left-full -translate-1/2 sm:size-30 2xl:size-50 -rotate-15 text-amber-500/30 -z-10" />
 				</div>
 				<div>
-					<p className="italic tracking-wider font-semibold text-pretty text-xl w-1/2 mx-auto text-center text-foreground/50">
+					<p className="italic tracking-wider font-semibold text-pretty sm:text-base 2xl:text-xl w-1/2 mx-auto text-center text-foreground/50">
 						Tu actual plan es el "Plan Profesional". ¿Deseas cambiar a plan
 						Empresarial? Checkea los beneficios de subir de plan.
 					</p>
 				</div>
-				<div className="flex gap-6 mt-20">
-					<Plan1 actualPlan={actualPlan} setActualPlan={setActualPlan} />
-					<Plan2 actualPlan={actualPlan} setActualPlan={setActualPlan} />
-					<Plan3 actualPlan={actualPlan} setActualPlan={setActualPlan} />
+				<div className="flex gap-6 sm:mt-10 2xl:mt-20">
+					{PLANS.map((plan, index) => (
+						<Plan
+							key={plan.title}
+							{...plan}
+							index={index}
+							actualPlan={actualPlan}
+							setActualPlan={setActualPlan}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
 	)
 }
 
-const Plan1 = ({
-	actualPlan,
-	setActualPlan,
-}: {
+interface PlanProps {
+	title: string
+	price: number
+	subtitle: string
+	benefits: string[]
+	index: number
 	actualPlan: 0 | 1 | 2
 	setActualPlan: (plan: 0 | 1 | 2) => void
-}) => {
-	return (
-		<div
-			className={`w-100 cardAccent p-20 py-10 flex flex-col items-start gap-4 ${actualPlan === 0 ? "border-2 border-green-600 scale-120" : ""}`}
-		>
-			<span className="text-3xl font-semibold tracking-widest">Gratis</span>
-			<span className="text-5xl font-semibold tracking-widest text-foreground/50">
-				$0
-			</span>
-			<span className="text-lg font-medium tracking-wider text-foreground/70">
-				Lorem ipsum dolor sit amet.
-			</span>
-			<div className="flex flex-col gap-2">
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 1</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 2</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 3</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 4</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 5</span>
-				</div>
-			</div>
-
-			<button
-				className={`${actualPlan !== 0 ? "themeBtnAccent" : "bg-background"} rounded-lg shadow-xl sm:text-sm 2xl:text-lg text-foreground tracking-widest px-6 py-2 cursor-pointer m-auto mt-10 w-full flex justify-center items-center gap-4`}
-				onClick={() => setActualPlan(0)}
-			>
-				{actualPlan === 0 ? (
-					<span>Actual Plan</span>
-				) : (
-					<>
-						<span>Adquirir Plan</span>
-						<ArrowRight className="size-5" />
-					</>
-				)}
-			</button>
-		</div>
-	)
 }
 
-const Plan2 = ({
+const Plan = ({
+	title,
+	price,
+	subtitle,
+	benefits,
+	index,
 	actualPlan,
 	setActualPlan,
-}: {
-	actualPlan: 0 | 1 | 2
-	setActualPlan: (plan: 0 | 1 | 2) => void
-}) => {
+}: PlanProps) => {
 	return (
 		<div
-			className={`w-100 cardAccent p-20 py-10 flex flex-col items-start gap-4 ${actualPlan === 1 ? "border-2 border-green-600 scale-120" : ""}`}
+			className={`sm:w-80 2xl:w-100 cardAccent sm:p-10 2xl:p-20 sm:py-6 2xl:py-10 flex flex-col items-start gap-4 ${actualPlan === index ? "border-2 border-green-600 scale-120" : ""}`}
 		>
-			<span className="text-3xl font-semibold tracking-widest">
-				Profesional
+			<span className="sm:text-xl 2xl:text-3xl font-semibold tracking-widest">
+				{title}
 			</span>
-			<span className="text-5xl font-semibold tracking-widest text-foreground/50">
-				$25
+			<span className="sm:text-3xl 2xl:text-5xl font-semibold tracking-widest text-foreground/50">
+				${price}
 			</span>
-			<span className="text-lg font-medium tracking-wider text-foreground/70">
-				Lorem ipsum dolor sit amet.
+			<span className="sm:text-base 2xl:text-lg font-medium tracking-wider text-foreground/70">
+				{subtitle}
 			</span>
 			<div className="flex flex-col gap-2">
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 1</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 2</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 3</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 4</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 5</span>
-				</div>
+				{benefits.map(benefit => (
+					<div key={benefit} className="flex gap-4">
+						<Check className="sm:size-5 2xl:size-6" />
+						<span className="sm:text-sm 2xl:text-base">{benefit}</span>
+					</div>
+				))}
 			</div>
 
 			<button
-				className={`${actualPlan !== 1 ? "themeBtnAccent" : "bg-background"} rounded-lg shadow-xl sm:text-sm 2xl:text-lg text-foreground tracking-widest px-6 py-2 cursor-pointer m-auto mt-10 w-full flex justify-center items-center gap-4`}
-				onClick={() => setActualPlan(1)}
+				className={`${actualPlan !== index ? "themeBtnAccent" : "bg-background"} rounded-lg shadow-xl sm:text-sm 2xl:text-lg text-foreground tracking-widest px-6 py-2 cursor-pointer m-auto sm:mt-4 2xl:mt-10 w-full flex justify-center items-center gap-4`}
+				onClick={() => setActualPlan(index as 0 | 1 | 2)}
 			>
-				{actualPlan === 1 ? (
-					<span>Actual Plan</span>
-				) : (
-					<>
-						<span>Adquirir Plan</span>
-						<ArrowRight className="size-5" />
-					</>
-				)}
-			</button>
-		</div>
-	)
-}
-
-const Plan3 = ({
-	actualPlan,
-	setActualPlan,
-}: {
-	actualPlan: 0 | 1 | 2
-	setActualPlan: (plan: 0 | 1 | 2) => void
-}) => {
-	return (
-		<div
-			className={`w-100 cardAccent p-20 py-10 flex flex-col items-start gap-4 ${actualPlan === 2 ? "border-2 border-green-600 scale-120" : ""}`}
-		>
-			<span className="text-3xl font-semibold tracking-widest">
-				Empresarial
-			</span>
-			<span className="text-5xl font-semibold tracking-widest text-foreground/50">
-				$55
-			</span>
-			<span className="text-lg font-medium tracking-wider text-foreground/70">
-				Lorem ipsum dolor sit amet.
-			</span>
-			<div className="flex flex-col gap-2">
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 1</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 2</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 3</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 4</span>
-				</div>
-				<div className="flex gap-4">
-					<Check className="size-6" />
-					<span>beneficio adquirido 5</span>
-				</div>
-			</div>
-
-			<button
-				className={`${actualPlan !== 2 ? "themeBtnAccent" : "bg-background"} rounded-lg shadow-xl sm:text-sm 2xl:text-lg text-foreground tracking-widest px-6 py-2 cursor-pointer m-auto mt-10 w-full flex justify-center items-center gap-4`}
-				onClick={() => setActualPlan(2)}
-			>
-				{actualPlan === 2 ? (
+				{actualPlan === index ? (
 					<span>Actual Plan</span>
 				) : (
 					<>
