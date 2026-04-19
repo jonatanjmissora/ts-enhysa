0f659bc64647890d775ef44b08c65ba2445211f4
Jonatan <jonatanjmissora@gmail.com>
2026-04-19 20:14:55 -0300

    informes del movil

--- src/routes/_protected/reports.tsx
+++ src/routes/_protected/reports.tsx
@@ -16,14 +16,14 @@ export const Route = createFileRoute("/_protected/reports")({
 function RouteComponent() {
 	return (
 		<section className="min-h-screen flex flex-col">
-			<header className="sm:text-base 2xl:text-xl font-semibold tracking-wider sm:h-20 2xl:h-24 px-20 bg-accent border border-background flex justify-between items-center">
+			<header className="hidden sm:text-base 2xl:text-xl font-semibold tracking-wider sm:h-20 2xl:h-24 px-20 bg-accent border border-background sm:flex justify-between items-center">
 				<span>Protocolo de Iluminación Res 84/12 SRT</span>
 				<PreferencesMenu />
 			</header>
-			<article className="flex flex-col gap-10 py-10 px-20">
-				<div className="flex justify-between items-center">
-					<p className=" text-2xl font-semibold">Reportes y Protocolos</p>
-					<div className="flex items-center justify-center gap-4">
+			<article className="flex flex-col gap-10 py-30 sm:py-10 px-6 sm:px-20">
+				<div className="flex flex-col sm:flex-row justify-between items-center gap-3">
+					<p className="w-full text-2xl font-semibold">Reportes y Protocolos</p>
+					<div className="flex items-center justify-end gap-4 ml-auto">
 						<span className="text-xl">Filtros</span>
 						<FileChartColumn className="sm:size-8 2xl:size-10 text-blue-600/50 cursor-pointer hover:text-blue-600 transition-colors" />
 						<CheckCircle2 className="sm:size-8 2xl:size-10 text-green-500/50 cursor-pointer hover:text-green-500 transition-colors" />
@@ -33,14 +33,14 @@ function RouteComponent() {
 				</div>
 
 				<div className="flex flex-col gap-4">
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<FileChartColumn className="sm:size-8 2xl:size-10 text-blue-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
-								<p className="sm:text-sm 2xl:text-base text-foreground/50">
+								<p className="text-xs sm:text-sm 2xl:text-base text-foreground/50">
 									Realizado el 15/05/2024
 								</p>
 							</div>
@@ -51,14 +51,14 @@ function RouteComponent() {
 						</div>
 					</div>
 
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<FileChartColumn className="sm:size-8 2xl:size-10 text-blue-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
-								<p className="sm:text-sm 2xl:text-base text-foreground/50">
+								<p className="text-xs sm:text-sm 2xl:text-base text-foreground/50">
 									Realizado el 15/05/2024
 								</p>
 							</div>
@@ -69,11 +69,11 @@ function RouteComponent() {
 						</div>
 					</div>
 
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<CheckCircle2 className="sm:size-8 2xl:size-10 text-red-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
 								<p className="sm:text-sm 2xl:text-base text-foreground/50">
@@ -87,14 +87,14 @@ function RouteComponent() {
 						</div>
 					</div>
 
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<FileChartColumn className="sm:size-8 2xl:size-10 text-blue-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
-								<p className="sm:text-sm 2xl:text-base text-foreground/50">
+								<p className="text-xs sm:text-sm 2xl:text-base text-foreground/50">
 									Realizado el 15/05/2024
 								</p>
 							</div>
@@ -105,11 +105,11 @@ function RouteComponent() {
 						</div>
 					</div>
 
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<Clock className="sm:size-8 2xl:size-10 text-amber-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
 								<p className="sm:text-sm 2xl:text-base text-foreground/50">
@@ -123,14 +123,14 @@ function RouteComponent() {
 						</div>
 					</div>
 
-					<div className="px-6 sm:py-4 2xl:py-6 cardAccent justify-between w-full">
+					<div className="px-6 py-4 2xl:py-6 cardAccent justify-between w-full">
 						<div className="flex gap-4 items-center">
 							<FileChartColumn className="sm:size-8 2xl:size-10 text-blue-700/70" />
 							<div className="flex flex-col gap-0">
-								<p className="sm:text-sm 2xl:text-lg font-semibold">
+								<p className="sm:text-sm 2xl:text-lg font-semibold w-40 sm:w-max truncate">
 									Empresa Metalurgica SA - Planta A
 								</p>
-								<p className="sm:text-sm 2xl:text-base text-foreground/50">
+								<p className="text-xs sm:text-sm 2xl:text-base text-foreground/50">
 									Realizado el 15/05/2024
 								</p>
 							</div>
