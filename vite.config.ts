a3f935e0ff9eb5e319a1bc69ba523e0848d228fd
Jonatan <jonatanjmissora@gmail.com>
2026-04-23 23:38:53 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,17 @@ import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import viteTsConfigPaths from "vite-tsconfig-paths"
-import { fileURLToPath, URL } from "url"
 import netlify from "@netlify/vite-plugin-tanstack-start"
 
 import tailwindcss from "@tailwindcss/vite"
 
-const config = defineConfig({
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
+export default defineConfig({
 	base: "/",
+	build: {
+		outDir: "dist",
+	},
 	plugins: [
 		devtools(),
-		// this is the plugin that enables path aliases
 		viteTsConfigPaths({
 			projects: ["./tsconfig.json"],
 		}),
@@ -27,5 +23,3 @@ const config = defineConfig({
 		viteReact(),
 	],
 })
-
-export default config
